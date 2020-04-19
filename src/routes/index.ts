d6c9994f4da52db31d1aa8a9e772877fc101dd64
Leonardo <leonardo@zungasoft.net>
2020-04-19 12:25:52 -0300

    Criaçao de registros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments_routes';
+import usersRouter from './users_routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
