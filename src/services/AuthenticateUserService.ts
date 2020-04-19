046a3b8efaba3e8cb6ae106222a0c340cd0055e2
Leonardo <leonardo@zungasoft.net>
2020-04-19 16:49:14 -0300

    validadndo credenciais

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('email/password incorretos.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('email/password incorretos.');
+    }
+    return { user };
+  }
+}
+
+export default AuthenticateUserService;
