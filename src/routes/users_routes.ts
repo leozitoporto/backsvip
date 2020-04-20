c6bb7d6d6bce9e85468594b792cbb666aa9b3a24
Leonardo <leonardo@zungasoft.net>
2020-04-20 15:01:36 -0300

    Atualizando avatar

--- src/routes/users_routes.ts
+++ src/routes/users_routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticate from '../middlewares/ensureAuthenticated';
 
@@ -36,8 +37,20 @@ usersRouter.patch(
   ensureAuthenticate,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
